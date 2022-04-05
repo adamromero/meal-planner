f1aeb28fd64f180adccafc0e4e5c3fdc6b6c1d54
adamromero <adamromero89@gmail.com>
2022-04-05 16:21:21 -0700

    Set up for production

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ app.use("/api/meals", require("./routes/mealRoutes"));
 
 if (process.env.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, "../client/dist")));
-   app.get("*", (req, res) => {
+   app.get("*", (req, res) =>
       res.sendFile(
          path.resolve(__dirname, "../", "client", "dist", "index.html")
-      );
-   });
+      )
+   );
 } else {
    app.get("/", (req, res) =>
       res.send("Please set environment variable to production")
@@ -28,3 +28,4 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, () => {
    console.log(`Listening on port ${port}`);
 });
+//
